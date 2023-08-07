00408f3f97552ef1eedb26fa310dc26bebde56b0
hyeong <mnihy3275@naver.com>
2023-08-07 15:36:31 -0500

    add complexity to gs

--- MLdebugger/model.py
+++ MLdebugger/model.py
@@ -11,7 +11,7 @@ class BiLSTM(torch.nn.Module):
                             num_layers=num_layers,
                             bidirectional=True,
                             batch_first=True)
-        
+    
     def forward(self, x):
         output, _ = self.lstm(x)
         return output
@@ -22,12 +22,36 @@ class GraphSAGE(torch.nn.Module):
         self.conv1 = SAGEConv(in_channels, hidden_channels)
         self.conv2 = SAGEConv(hidden_channels, hidden_channels)
 
+        # nn skip connections
+        self.skip1 = nn.Sequential(
+            nn.Linear(in_channels, hidden_channels // 2), 
+            nn.ReLU(),
+            nn.Linear(hidden_channels // 2, hidden_channels)
+        )
+        self.skip2 = nn.Sequential(
+            nn.Linear(hidden_channels, hidden_channels // 2),
+            nn.ReLU(),
+            nn.Linear(hidden_channels // 2, hidden_channels)
+        )
+
+        # BN layers
+        self.bn1 = nn.BatchNorm1d(hidden_channels)
+        self.bn2 = nn.BatchNorm1d(hidden_channels)
+
+        # dropout
+        self.dropout = nn.Dropout(0.5)
+
     def forward(self, x, edge_index):
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
-        x = self.conv2(x, edge_index)
-        x = F.relu(x)
-        return x
+        x1 = self.conv1(x, edge_index)
+        x1 = self.bn1(x1)
+        x1 = F.relu(x1 + self.skip1(x))
+        x1 = self.dropout(x1)
+
+        x2 = self.conv2(x1, edge_index)
+        x2 = self.bn2(x2)
+        x2 = F.relu(x2 + self.skip2(x1))
+        x2 = self.dropout(x2)
+        return x2
 
 class CombinedModel(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, num_layers=1):
